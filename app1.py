ac877b11b4bf5c9d2c31d1d2dec5d7ec490cabff
Fernando Ferraz Ribeiro <ffribeiro@gmai.com>
2021-05-30 10:10:22 -0300

    read_newes in main fila

--- app1.py
+++ app1.py
@@ -13,10 +13,48 @@ import pandas as pd
 import os
 import zipfile
 
-from read_newest import Read_newest
+import patoolib
 #import requests
 
 list_dia_semana = ['dom', 'seg', 'ter', 'qua', 'qui', 'sex', 'sab']
+
+def Read_newest(path):
+
+    files = os.listdir(path)
+    paths = [os.path.join(path, basename) for basename in files]
+    paths = sorted(paths, key=os.path.getctime)
+    newest_file = paths[-1]
+    [os.remove(i) for i in paths[:-1]]
+    # exttensao dos arquivos
+    file_ext = newest_file.split('.')[-1]
+    # dataframe vazio
+    df = pd.DataFrame()
+
+    # caso arquivo rar 
+    if file_ext.lower() in ['rar']:
+        ext_old = len(os.path.basename( newest_file).split('.')[-1])
+        new_file = path + os.path.basename( newest_file)[:-(ext_old)] + "zip"
+        patoolib.repack_archive(newest_file, new_file )
+        os.remove(newest_file)
+        newest_file = new_file
+
+    comp_file = zipfile.ZipFile(newest_file) 
+    # para cada aquivo na lista de informacoes do atquivo mais novo...
+
+    for file_info in comp_file.infolist():
+    #abrir o arquivo
+        with comp_file.open(file_info) as comp_internal_file:
+        #Ler o csv
+            df_part = pd.read_csv(comp_internal_file, sep=';')
+            df = pd.concat([df, df_part], ignore_index=True)
+            
+    # Prepoc datetime
+    cat_dia_semana = pd.CategoricalDtype(categories= list_dia_semana , ordered=True)
+    df['data'] = pd.to_datetime(df['data'],format="%Y-%m-%d")
+    df['dia_semana'] = df['data'].dt.dayofweek
+    df['dia_semana_nm'] = df['dia_semana'].replace({0:'seg', 1: 'ter', 2:'qua', 3:'qui', 4:'sex', 5:'sab', 6:'dom'}).astype(cat_dia_semana)
+
+    return df
 
 df = Read_newest('./data/')
 
