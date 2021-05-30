75f94c22c708f9821dde764443aaba8764a52616
Fernando Ferraz Ribeiro <ffribeiro@gmai.com>
2021-05-30 14:14:09 -0300

    testando importar arquivo read_newest

--- app1.py
+++ app1.py
@@ -15,36 +15,7 @@ from dash.dependencies import Input, Output
 
 import plotly.graph_objs as go
 
-
-def Read_newest(path):
-
-    files = os.listdir(path)
-    paths = [os.path.join(path, basename) for basename in files]
-    paths = sorted(paths, key=os.path.getctime)
-    newest_file = paths[-1]
-    [os.remove(i) for i in paths[:-1]]
-    # exttensao dos arquivos
-    file_ext = newest_file.split('.')[-1]
-    # dataframe vazio
-    df = pd.DataFrame()
-
-    comp_file = zipfile.ZipFile(newest_file) 
-    # para cada aquivo na lista de informacoes do atquivo mais novo...
-
-    for file_info in comp_file.infolist():
-    #abrir o arquivo
-        with comp_file.open(file_info) as comp_internal_file:
-        #Ler o csv
-            df_part = pd.read_csv(comp_internal_file, sep=';')
-            df = pd.concat([df, df_part], ignore_index=True)
-            
-    # Prepoc datetime
-    cat_dia_semana = pd.CategoricalDtype(categories= list_dia_semana , ordered=True)
-    df['data'] = pd.to_datetime(df['data'],format="%Y-%m-%d")
-    df['dia_semana'] = df['data'].dt.dayofweek
-    df['dia_semana_nm'] = df['dia_semana'].replace({0:'seg', 1: 'ter', 2:'qua', 3:'qui', 4:'sex', 5:'sab', 6:'dom'}).astype(cat_dia_semana)
-
-    return df
+from read_newest import Read_newest
 
 list_dia_semana = ['dom', 'seg', 'ter', 'qua', 'qui', 'sex', 'sab']
 
