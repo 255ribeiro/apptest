3b89ad7c1c8994b1134854017dce9a701e974f82
Fernando Ferraz Ribeiro <ffribeiro@gmai.com>
2021-05-30 10:23:29 -0300

    eliminate unused import (plotly.express)

--- app1.py
+++ app1.py
@@ -4,7 +4,6 @@ import dash_html_components as html
 from dash.dependencies import Input, Output
 
 import plotly.graph_objs as go
-import plotly.express as px
 
 import numpy as np
 import pandas as pd
